afa71b1a62519f81bc123ad0767047132a7bbb44
snailvoyager <kyj88514@gmail.com>
2023-03-23 00:26:24 +0900

    Abstract Factory Pattern

--- DesignPatterns/src/chap04FactoryPattern/af/NYPizzaStore.java
+++ DesignPatterns/src/chap04FactoryPattern/af/NYPizzaStore.java
@@ -1,14 +1,27 @@
 package chap04FactoryPattern.af;
 
+import chap04FactoryPattern.af.ingredient.Veggies;
+
 public class NYPizzaStore extends PizzaStore{
     @Override
     protected Pizza createPizza(String item) {
         Pizza pizza = null;     //Abstract Product
-        PizzaIngredientFactory ingredientFactory = new NYPizzaIngredientFactory();  //뉴욕가게에는 뉴욕공장을 연결
+        PizzaIngredientFactory ingredientFactory = new NYPizzaIngredientFactory();  //뉴욕가게에는 뉴욕공장을 연결 (구상클래스에 의존적?)
 
-        if(item.equals("cheese")) {
-            pizza = new CheesePizza(ingredientFactory);     //피자를 만들 때 필요한 공장을 전달
-            pizza.setName("New York Style Cheese Pizza");
+        switch(item) {
+            case "cheese" :
+                pizza = new CheesePizza(ingredientFactory);     //피자를 만들 때 필요한 공장을 전달
+                pizza.setName("New York Style Cheese Pizza");
+                break;
+            case "veggie" :
+                //pizza = new Veggies(ingredientFactory);
+                break;
+            case "clam" :
+                //pizza = new Clams(ingredientFactory);
+                break;
+            case "pepperoni" :
+                //pizza = new Pepperoni(ingredientFactory);
+                break;
         }
 
         return pizza;
